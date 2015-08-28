a4ae07b8345784c793618d84cd103b1c474bb378
Jan Matas <jm6214@ic.ac.uk>
2015-08-28 16:31:07 +0200

    object assignenment completed

--- tracked_object.py
+++ tracked_object.py
@@ -1,37 +1,76 @@
-from kalman import Kalman2D
+
+from __future__ import division
+from collections import deque
+import random
+MAX_HISTORY = 15
+MIN_DISTANCE_OF_PASS = 250
 class TrackedObject():
-    def __init__(self, x, y):
+
+    def __init__(self, x, y, time):
+        self.history = deque()
+        self.frames_since_start = 0
+        self.update (x, y, time)
+        self.frames_missing = 0
         self.start_x = x
         self.start_y = y
-        self.x = x
-        self.y = y
-        self.started = True
-        self.kalman = False
 
-    def update (self, x, y):
-        self.x = x
-        self.y = y
-        if self.started:
-            print "start"
-            v_x = x - self.start_x
-            v_y = y - self.start_y
-            print v_x, v_y
-            self.kalman = Kalman2D(x, y, v_x, v_y)
-            self.started = False
-        else:
-            print x, y
-            self.kalman.predict_and_correct(x, y)
+        self.color = (255*random.random(), 255*random.random(), 255*random.random())
 
-    def update_missing(self):
-        if not self.started:
-            self.kalman.predict()
 
+
+    def update (self, x, y, time):
+
+        position = (x, y, time)
+        if len(self.history) > MAX_HISTORY:
+            self.history.popleft()
+        self.history.append(position)
+        self.frames_missing = 0
+        self.frames_since_start += 1
 
     def get_position(self):
-        return (self.x, self.y)
+        if (len(self.history) == 0) :
+            return (0,0)
+        return (self.history[-1][0], self.history[-1][1])
 
-    def get_kalman(self):
-        if self.kalman:
-            return self.kalman.get_prediction()
+    def missing(self):
+        self.frames_missing += 1
+        print "missing: " + str(self.frames_missing)
+        if self.frames_missing > 10 or self.frames_since_start < 4:
+            return -1
         else:
-            return self.get_position()
+            return 0
+
+    def get_direction(self):
+        _, y = self.get_position()
+        if (abs(y - self.start_y) > MIN_DISTANCE_OF_PASS):
+            if y > self.start_y:
+                return 1
+            return -1
+        return 0
+
+
+
+    def get_prediction(self, current_t):
+        if (len(self.history) == 0) :
+            return (0,0)
+        if (len(self.history) == 1) :
+            return (self.history[0][0],self.history[0][1])
+        start_x = self.history[0][0]
+        start_y = self.history[0][1]
+        start_t = self.history[0][2]
+        last_x = self.history[-1][0]
+        last_y = self.history[-1][1]
+        last_t = self.history[-1][2]
+
+        time_since_start = last_t - start_t
+
+        v_x = (last_x - start_x) / time_since_start
+        v_y = (last_y - start_y) / time_since_start
+
+        delta_t = current_t - last_t
+
+        current_x = last_x + v_x * delta_t
+
+        current_y = last_y + v_y * delta_t
+
+        return (int(current_x), int(current_y))
--- tracking.py
+++ tracking.py
@@ -1,12 +1,14 @@
 import numpy as np
 import cv2
 import time
+import math
 from tracked_object import TrackedObject
-URL = '/home/jancio/Desktop/portals-kinect/data/video.avi'
-MIN_HEIGHT = 70
+URL = '/home/jancio/Desktop/video.avi'
+MIN_HEIGHT = 50
 GUI = True
-MIN_CONTOUR_AREA = 100
+MIN_CONTOUR_AREA = 4000
 HEIGHT_TOLERANCE = 10
+MAX_DISTANCE_TO_PARSE =  300
 
 
 def filter_frame(frame, bg_reference):
@@ -20,11 +22,11 @@ def filter_frame(frame, bg_reference):
         #Threshold the foreground
         ret, thresh = cv2.threshold(fg, MIN_HEIGHT, 255, cv2.THRESH_BINARY)
         #Execute erode and dilate functions to eliminate noise
-        #erode_kernel = np.ones((5,5),np.uint8)
-        #dilate_kernel = np.ones((10,10),np.uint8)
-        #erosion = cv2.erode(thresh, erode_kernel,iterations = 1)
-        #dilatation = cv2.erode(erosion, dilate_kernel,iterations = 1)
-        return thresh
+        erode_kernel = np.ones((5,5),np.uint8)
+        dilate_kernel = np.ones((20,20),np.uint8)
+        erosion = cv2.erode(thresh, erode_kernel,iterations = 1)
+        dilatation = cv2.erode(erosion, dilate_kernel,iterations = 1)
+        return dilatation
 
 
 def find_contour_centroids(frame, filtered_fg):
@@ -41,7 +43,6 @@ def find_contour_centroids(frame, filtered_fg):
         centroids = []
         # Iterate over valid contours
         for h,cnt in enumerate(valid_contours):
-
             # Create an empty mask
             mask = np.zeros(filtered_fg.shape,np.uint8)
             # Draw a contour on the mask
@@ -54,11 +55,11 @@ def find_contour_centroids(frame, filtered_fg):
             # And the threshold with mask to eliminate all results not in the
             # same contour
             result = cv2.bitwise_and(mask, thresh)
-            # Find contour on a result to be able to find centroid
+            # Find biggest contour on a result
             _, contours, _ = cv2.findContours(result,
                     cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             max_cnt = max(contours, key=cv2.contourArea)
-
+            # Check if the biggest contour has no-zero area
             if (cv2.contourArea(max_cnt) > 0):
                 # Find moments of the found contour
                 M = cv2.moments(max_cnt)
@@ -70,14 +71,90 @@ def find_contour_centroids(frame, filtered_fg):
         return centroids
 
 
+def compute_distance(a, b):
+    # Function returns the distance between two (x, y) points
+    return math.hypot(b[0] - a[0], b[1] - a[1])
+
+
+def assign_centroids(tracked_objects, centroids, t):
+    # Function tries to assign every existing tracked object to a corresponding
+    # centroid (contour). It returns the pairs of (objID, contourID) and lists
+    # of unused objects and unused centroids
+
+    # Compute all distances between objects and centroids
+    distances = []
+    for centroidID, centroid in enumerate(centroids):
+        for objID, obj in enumerate(tracked_objects):
+            distance = compute_distance(centroid, obj.get_prediction(t))
+            distances.append((objID, centroidID, distance))
+    # Filter out those that are greater than maximal distance to parse
+    distances = filter(lambda d : d[2] < MAX_DISTANCE_TO_PARSE, distances)
+    # Sort them in ascending order, so the small distances get prioritized
+    distances = sorted(distances, key=lambda d: d[2])
+
+    # Initialise empty lists
+    used_objects = []
+    used_centroids = []
+    pairs = []
+
+    # Find the pairs
+    for distance in distances:
+        objID, centroidID, _ = distance
+        # Do not reuse centroids or objects
+        if objID not in used_objects and centroidID not in used_centroids:
+            pairs.append((objID, centroidID))
+            used_centroids.append(centroidID)
+            used_objects.append(objID)
+
+    # find the list of unused objects
+    unused_objects = list(tracked_objects)
+    for used_object_index in sorted(used_objects, reverse=True):
+        del unused_objects[used_object_index]
+
+    # find the list of unused centroids
+    unused_centroids = list(centroids)
+    for used_centroid_index in sorted(used_centroids, reverse=True):
+        del unused_centroids[used_centroid_index]
+
+    return pairs, unused_objects, unused_centroids
+
+
 def is_valid_contour(contour):
     # Function decides if the contour is good enough to be tracked
     contour_area = cv2.contourArea(contour)
     return contour_area > MIN_CONTOUR_AREA
 
+def create_objects(unused_centroids, tracked_objects, t):
+    # Function creates a new object for each unasigned centroid
+    for centroid in unused_centroids:
+        new_obj = TrackedObject(centroid[0], centroid[1], t)
+        tracked_objects.append(new_obj)
+
+
+def update_pairs(pairs, tracked_objects, centroids, t):
+    # Function writes the current measurment into the object
+    for pair in pairs:
+        obj = tracked_objects[pair[0]]
+        x, y = centroids[pair[1]]
+        obj.update(x, y, t)
+
+
+def update_missing(unused_objects, tracked_objects, counter_in, counter_out):
+    # Function up
+    for unused_object in unused_objects:
+        if unused_object.missing() == -1:
+            if unused_object.get_direction() == 1:
+                counter_in += 1
+            if unused_object.get_direction() == -1:
+                counter_out += 1
+            tracked_objects.remove(unused_object)
+    return counter_in, counter_out
+
 
 def tracking_start():
     frame_delay = 50
+    counter_in = 0
+    counter_out = 0
     # Initialise videl capture
     cap = cv2.VideoCapture(URL)
     # Take first frame as bacground reference
@@ -85,8 +162,9 @@ def tracking_start():
     img_grayscale = cv2.cvtColor(initial_frame,cv2.COLOR_BGR2GRAY)
     bg_reference = cv2.blur(img_grayscale,(5,5))
     # Iterate forever
-    obj = False
+    tracked_objects = []
     while(cap.isOpened()):
+        t = cv2.getTickCount()
         #Read frame
         ret, frame = cap.read()
         if not ret:
@@ -98,33 +176,44 @@ def tracking_start():
         # Find centroids of all contours
         centroids = find_contour_centroids(frame, filtered_fg)
 
-        if (len(centroids) > 0) :
-            if obj:
-                obj.update(centroids[0][0], centroids[0][1])
-            else:
-                obj = TrackedObject(centroids[0][0], centroids[0][1])
-        else :
-            obj = False
+        pairs, unused_objects, unused_centroids = assign_centroids(
+                tracked_objects, centroids, t)
+
 
         if GUI:
-            red = (255, 0 ,0)
-            radius = 10
-            for i, centroid in enumerate(centroids):
+            # Draw found objects - big circle is object, small circle is
+            # prediction of its position
+            for pair in pairs:
+                obj = tracked_objects[pair[0]]
+                centroid = centroids[pair[1]]
+                cv2.circle(frame, centroid, 10, obj.color, -1)
+                cv2.circle(frame, obj.get_prediction(t), 3, obj.color, -1)
+            # Draw objects that were not found as black circles
+            for unused_object in unused_objects:
+                cv2.circle(frame, unused_object.get_prediction(t), 10, (0,0,0),
+                    -1)
+        # Create objects for centroid that were not assigned
+        create_objects(unused_centroids, tracked_objects, t)
+        # Update assigned centroid with current measurements
+        update_pairs(pairs, tracked_objects, centroids, t)
+        # Delete missing objects and increment counters
+        counter_in, counter_out = update_missing(unused_objects,
+                tracked_objects, counter_in, counter_out)
 
-
-                cv2.circle(frame, centroid, radius, red)
-            if obj:
-
-                cv2.circle(frame, (obj.get_kalman()[0], obj.get_kalman()[1]),radius,(0,255,0))
+        if GUI:
+            # Show counters
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame,str(counter_out),(10,50), font, 1,(0,0,255),2)
+            cv2.putText(frame,str(counter_in),(10,400), font, 1,(0,0,255),2)
             cv2.imshow('frame',frame)
-            #cv2.imshow('fg', filtered_fg)
-
+            cv2.imshow('fg',filtered_fg)
             key = cv2.waitKey(frame_delay)
 
             if key & 0xFF == ord('q'):
                 break
             if key & 0xFF == ord('p'):
                 time.sleep(2)
+            # Allow user to control speed
             if key & 0xFF == ord('s'):
                 frame_delay = 1000
             if key & 0xFF == ord('n'):
@@ -133,7 +222,7 @@ def tracking_start():
                 frame_delay = 1
 
 
-
+    # Teardown
     cap.release()
     if GUI:
         cv2.destroyAllWindows()
